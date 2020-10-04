b85b8886b432538901f3d93e4cc23358eabd9005
Kim Sang Chul <tsb03672@naver.com>
2020-10-04 23:24:06 +0900

    [NavigationBar] Head Navigation bar added

---
+++ src/Components/NavigationBar/NavigationBar.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const MenuWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const MainLogo = styled.div`
+    img {
+        height: 60px;
+    }
+`;
