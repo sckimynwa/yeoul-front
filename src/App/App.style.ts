da7061fb160dea5eafefbdf1b780de6efe33aa52
Kim Sang Chul <tsb03672@naver.com>
2020-10-04 23:23:55 +0900

    [Layout] styled component added & layout added

---
+++ src/App/App.style.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import { Layout } from 'antd';
+const { Header, Content, Footer } = Layout;
+
+export const MainLayout = styled(Layout)`
+    height: 100vh;
+`;
+
+export const MainHeader = styled(Header)`
+    background: white;
+    position: fixed;
+    z-index: 1000;
+    width: 100%;
+`;
+
+export const MainContent = styled(Content)`
+    background: white;
+`;
+
+export const MainFooter = styled(Footer)`
+    text-align: center;
+    background: white;
+`;
--- src/App/AppPresenter.tsx
+++ src/App/AppPresenter.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import NavigationBar from '../Components/NavigationBar';
+import { MainContent, MainLayout, MainHeader, MainFooter } from './App.style';
 
-const AppPresenter: React.FC = () => (<>APP</>)
+const AppPresenter: React.FC = () => (
+    <MainLayout>
+        <MainHeader>
+            <NavigationBar />
+        </MainHeader>
+        <MainContent></MainContent>
+        <MainFooter>NormalCoder ©2020 Created by Yeoul.</MainFooter>
+    </MainLayout>
+)
 
 export default AppPresenter;
