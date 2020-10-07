03d2bb862c1c26bdc72492d335c5dd8380bded6d
Kim Sang Chul <tsb03672@naver.com>
2020-10-07 23:39:23 +0900

    [Navigation Bar] Nav bar fixed

--- src/Components/NavigationBar/NavigationBar.tsx
+++ src/Components/NavigationBar/NavigationBar.tsx
@@ -4,26 +4,28 @@ import { Menu } from "antd";
 import { MenuWrapper, MainLogo } from "./NavigationBar.style";
 
 const NavigationBar: React.FC = () => (
-  <MenuWrapper>
-    <MainLogo>
-      <img src={mainLogo} />
-    </MainLogo>
-    <Menu mode="horizontal">
-      <Menu.Item key="about">About</Menu.Item>
-      <Menu.Item key="coding">
-        <a href="https://yeoulcoding.tistory.com">Coding.</a>
-      </Menu.Item>
-      <Menu.Item key="design">
-        <a href="https://yeouldesign.tistory.com">Design.</a>
-      </Menu.Item>
-      <Menu.Item key="writing">
-        <a href="https://yeoulwriting.tistory.com">Writing.</a>
-      </Menu.Item>
-      <Menu.Item key="economics">
-        <a href="https://yeouleconomist.tistory.com">Economy.</a>
-      </Menu.Item>
-    </Menu>
-  </MenuWrapper>
-);
+    <MenuWrapper>
+        <MainLogo>
+            <img src={mainLogo} />
+        </MainLogo>
+        <Menu mode="horizontal">
+            <Menu.Item key="about">
+                About
+            </Menu.Item>
+            <Menu.Item key="coding">
+                <a href="https://yeoulcoding.tistory.com">Coding.</a>
+            </Menu.Item>
+            <Menu.Item key="design">
+                <a href="https://yeouldesigner.tistory.com">Design.</a>
+            </Menu.Item>
+            {/* <Menu.Item key="writing">
+                <a href="https://yeoulwriting.tistory.com">Writing.</a>
+            </Menu.Item> */}
+            <Menu.Item key="economics">
+                <a href="https://yeouleconomist.tistory.com">Economy.</a>
+            </Menu.Item>
+        </Menu>
+    </MenuWrapper>
+)
 
 export default NavigationBar;
