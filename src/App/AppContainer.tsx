ef8f5e9a48870b49645fb6ccc3bb72b084442bc3
Kim Sang Chul <tsb03672@naver.com>
2020-11-07 20:11:27 +0900

    [DarkMode] Default Dark Mode Settings

--- src/App/AppContainer.tsx
+++ src/App/AppContainer.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import AppPresenter from './AppPresenter';
+import { ThemeType } from './AppModel';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from '../Utils/Themes';
+import { GlobalStyles } from '../Utils/GlobalStyles';
 
 const AppContainer: React.FC = () => {
-    return <AppPresenter />
+    const [theme, setTheme] = useState<ThemeType>('light');
+    const toggleTheme = useCallback(() => {
+        setTheme((theme: ThemeType) => theme === 'light' ? 'dark' : 'light')
+    }, [])
+
+    console.log(theme);
+
+    return (<>
+        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+            <GlobalStyles />
+            <AppPresenter />
+            <button onClick={toggleTheme}>ontoggle</button>
+        </ThemeProvider>
+    </>)
 }
 
 export default AppContainer;
